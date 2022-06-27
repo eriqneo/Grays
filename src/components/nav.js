30dc392b08e369a369d0b956413cc88758d054ae
eriqneo <eriqneone@gmail.com>
2022-06-27 07:58:30 +0300

    added contact us

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,6 +3,15 @@
 navTemplate.innerHTML = `
        <link href="/dist/output.css" rel="stylesheet">
        <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+       <style>
+      .mobile-menu-button:not(:checked) ~ .mobile-menu {
+             display: none;
+        }
+
+          .mobile-menu-button:checked ~.mobile-menu {
+                display: block;
+                  }
+      </style>
       <nav class="bg-blue-800 shadow-lg sticky top-0 z-50">
       <div class="max-w-6xl px-2 py-4">
           <div class="flex justify-between">
@@ -31,14 +40,15 @@ navTemplate.innerHTML = `
                       </a>
                       <a href="clinics.html" class="py-4 px-1 text-gray-300 text-xl font-semibold hover:text-white hover:border-b-2 transition duration-300">Special Clinics</a>
                       <a href="" class="py-4 px-1 text-gray-300 text-xl font-semibold hover:text-white hover:border-b-2 transition duration-300">Specialists</a>
-                      <a href="" class="py-4 px-1 text-gray-300 text-xl font-semibold hover:text-white hover:border-b-2 transition duration-300">Contact Us</a>
+                      <a href="contact.html" class="py-4 px-1 text-gray-300 text-xl font-semibold hover:text-white hover:border-b-2 transition duration-300">Contact Us</a>
                   </div>
                   <div class="hidden md:flex items-center whitespace-nowrap">
                       <a href="" class="py-2 px-2 ml-20 font-medium text-gray-800 bg-white rounded hover:bg-black hover:text-white transition duration-300">Book Appointment</a>
                   </div>
                   <!--MObile Menu Button-->
                   <div class="md:hidden flex items-center">
-                      <button class="outline-none mobile-menu-button">
+                      <button class="outline-none mobile-menu-button"
+                      id="mobile-menu-button">
                           <svg class=" w-6 h-6 text-gray-200 hover:text-white "
 							x-show="!showMenu"
 							fill="none"
@@ -55,7 +65,7 @@ navTemplate.innerHTML = `
               </div>
           </div>
           <!--Mobile Menu-->
-          <div class="hidden mobile-menu">
+          <div class="md:hidden mobile-menu block">
               <ul class="">
                   <li class="active"><a href="index.html" class="block text-xl px-2 py-4 text-white bg-black font-semibold">Home</a></li>
 				  <li><a href="#services" class="block py-4 px-2 text-gray-300 text-sm font-semibold hover:text-white hover:border-b-2 transition duration-300">About Us</a></li>
